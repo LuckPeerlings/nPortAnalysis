53710ba873b8c0df3f95bb697fc7bd93fbba3461
Luck Peerlings <luck@kth.se>
2016-07-29 08:32:46 +0200

    Fixed small errors to the uncertainty analysis

--- @UncertaintyAnalysisNPort/UncertaintyAnalysisNPort.m
+++ @UncertaintyAnalysisNPort/UncertaintyAnalysisNPort.m
@@ -22,15 +22,15 @@ classdef UncertaintyAnalysisNPort < MultiVariateAnalysis
         end
         
         function displayScatMatrix(obj)
-            
             figure;
-            
             for ii = 1:obj.ClassHandle.NrPorts
                 for jj = 1:obj.ClassHandle.NrPorts
                     ScatUV = obj.Output.ScatNPort.(['S',num2str(jj),num2str(ii)]);
-                    
-                    TotVar = squeeze(sum(ScatUV.Var,1) + sum(ScatUV.CorrVar,1));
-                    
+                    if isempty(ScatUV.CorrVar)
+                        TotVar = squeeze(sum(ScatUV.Var,1));
+                    else
+                        TotVar = squeeze(sum(ScatUV.Var,1) + sum(ScatUV.CorrVar,1));
+                    end
                     %Calculate the variance matrix rotated in the direction of
                     %the mean vector value.
                     %Taken from In-Phase/Quadrature Covariance-Matrix
@@ -56,15 +56,67 @@ classdef UncertaintyAnalysisNPort < MultiVariateAnalysis
                     ScatUV = obj.Output.ScatNPort.(['S',num2str(jj),num2str(ii)]);
                     subplot(obj.ClassHandle.NrPorts,obj.ClassHandle.NrPorts, (ii - 1)*obj.ClassHandle.NrPorts + jj )
                     hold on
-                    plot(obj.ClassHandle.FreqVec, angle( ScatUV.Value) + 2*sqrt(AlignedVar(4,:))./abs( ScatUV.Value),'k-');
-                    plot(obj.ClassHandle.FreqVec, angle( ScatUV.Value),'-');
-                    plot(obj.ClassHandle.FreqVec, angle( ScatUV.Value) - 2*sqrt(AlignedVar(4,:))./abs( ScatUV.Value),'k-');
+                    plot(obj.ClassHandle.FreqVec, unwrap(angle( ScatUV.Value)) + 2*sqrt(AlignedVar(4,:))./abs( ScatUV.Value),'k-');
+                    plot(obj.ClassHandle.FreqVec, unwrap(angle( ScatUV.Value)),'-');
+                    plot(obj.ClassHandle.FreqVec, unwrap(angle( ScatUV.Value)) - 2*sqrt(AlignedVar(4,:))./abs( ScatUV.Value),'k-');
                     
                     
                 end
             end
         end
-        function VarianceDistribution(obj)
+        function VarianceDistribution_RealImag(obj)
+            figure
+            cc = 1;
+            for ii = 1:obj.ClassHandle.NrPorts
+                for jj = 1:obj.ClassHandle.NrPorts
+                    h1(cc) = subplot(obj.ClassHandle.NrPorts,obj.ClassHandle.NrPorts, (ii - 1)*obj.ClassHandle.NrPorts + jj );
+                    ScatUV = obj.Output.ScatNPort.(['S',num2str(jj),num2str(ii)]);
+                    assignin('base','ScatUV',ScatUV)
+                    %Get the unique groups
+                    UniqueGroupNames = unique([ScatUV.Group{:}]);
+                    SummedVar = zeros(length(UniqueGroupNames),size(ScatUV.Var,2),4);
+                    for ll = 1:size(ScatUV.Var,1);
+                        %Find the index of the position in the unique names
+                        I = find(strcmp(UniqueGroupNames,ScatUV.Group{ll}));
+                        SummedVar(I,:,:) = SummedVar(I,:,:) + ScatUV.Var(ll,:,:);
+                    end
+                    RelativeVar = bsxfun(@rdivide,SummedVar(:,:,1),sum(SummedVar(:,:,1),1));
+                    area(h1(cc),obj.ClassHandle.FreqVec,RelativeVar.')
+                    if ii == 1 && jj == 1
+                        legend(UniqueGroupNames)
+                        title('Relative Contribution of variance to the magnitude uncertainty (Uncorrelated Errors)')
+                    end
+                    cc = cc +1;
+                end
+            end
+            
+            
+            figure  
+            cc = 1;
+            for ii = 1:obj.ClassHandle.NrPorts
+                for jj = 1:obj.ClassHandle.NrPorts
+                    h2(cc) = subplot(obj.ClassHandle.NrPorts,obj.ClassHandle.NrPorts, (ii - 1)*obj.ClassHandle.NrPorts + jj );
+                    ScatUV = obj.Output.ScatNPort.(['S',num2str(jj),num2str(ii)]);
+                    %Get the unique groups
+                    UniqueGroupNames = unique([ScatUV.Group{:}]);
+                    SummedVar = zeros(length(UniqueGroupNames),size(ScatUV.Var,2),4);
+                    for ll = 1:size(ScatUV.Var,1);
+                        %Find the index of the position in the unique names
+                        I = find(strcmp(UniqueGroupNames,ScatUV.Group{ll}));
+                        SummedVar(I,:,:) = SummedVar(I,:,:) + ScatUV.Var(ll,:,:);
+                    end
+                    RelativeVar = bsxfun(@rdivide,SummedVar(:,:,4),sum(SummedVar(:,:,4),1));
+                    area(h2(cc), obj.ClassHandle.FreqVec,RelativeVar.')
+                    if ii == 1 && jj == 1
+                        legend(UniqueGroupNames)
+                        title('Relative Contribution of variance to the phase uncertainty (Uncorrelated Errors)')
+                    end
+                    cc = cc + 1;
+                end
+            end
+        end    
+        
+        function VarianceDistribution_AbsAngle(obj)
             figure
             cc = 1;
             for ii = 1:obj.ClassHandle.NrPorts
