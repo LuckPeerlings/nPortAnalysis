c33d7824e9c319f2383508aa0bfdafd07c12413c
Luck Peerlings <luck@kth.se>
2017-01-25 18:09:35 +0100

    Added the calculation of the energy dissipated per port and the calculation of the transfer matrix

--- @NPortAnalysis/NPortAnalysis.m
+++ @NPortAnalysis/NPortAnalysis.m
@@ -5,8 +5,8 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
     end
     
     properties
-        NrPorts = 1;
-        NrMeas = 1;
+        NrPorts = 2;
+        NrMeas = 2;
         InputChecked = true;
         ScatNPort
     end
@@ -29,7 +29,7 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
                 error('The number of measurements per port are not equal for all the ports')
             end
             
-            %And check if there is enough independent measurements
+            %And check if there are enough independent measurements
             if  NrPorts > NrMeas
                 error('The number of independent measurements is smaller than the number of ports')
             end
@@ -47,7 +47,6 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
                         obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]) = MergeStructs( obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]), obj.Input.(['Port',num2str(ii)]).Constant );
                         
                     end
-                    
                     GasProperties_chk(  obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]).GasProp)
                     obj.WaveNumber_chk( obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]).WaveNumberProp, ...
                         'GasProp',obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]).GasProp,...
@@ -62,8 +61,94 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
             obj.NrPorts  = NrPorts;
             obj.NrMeas = NrMeas;
             obj.InputChecked = true;
-            
         end
+        
+        function obj = EnergyDissipationPerPort(obj,varargin)
+            for ii = 1:obj.NrPorts
+                for jj = 1:obj.NrMeas
+                    if isfield(obj.Input.(['Port',num2str(ii)]),'Constant')
+                        InputDecomp.(['Port',num2str(ii)]).(['Meas',num2str(jj)]) = MergeStructs( obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]), obj.Input.(['Port',num2str(ii)]).('Constant') );
+                    else
+                        InputDecomp.(['Port',num2str(ii)]).(['Meas',num2str(jj)]) = obj.Input.(['Port',num2str(ii)]).(['Meas',num2str(jj)]);
+                    end
+                end
+            end
+            %Obtain the mean flow velocity for the ports.
+            MachNumberPort = zeros(1,obj.NrPorts);
+            for ii = 1:obj.NrPorts
+                for jj = 1:obj.NrMeas
+                    Properties = GasProperties(InputDecomp.(['Port',num2str(ii)]).(['Meas',num2str(jj)]).GasProp);
+                    %Take the average of the the measured speeds
+                    if isfield(InputDecomp.(['Port',num2str(ii)]).(['Meas',num2str(jj)]).WaveNumberProp,'U')
+                        MachNumberPort(ii) = MachNumberPort(ii) + InputDecomp.(['Port',num2str(ii)]).(['Meas',num2str(jj)]).WaveNumberProp.U/mean(Properties.SpeedOfSound)/obj.NrMeas;
+                    end
+                end
+                Radius(ii) = InputDecomp.(['Port',num2str(ii)]).Meas1.WaveNumberProp.Model.r;
+            end
+            %Create the matrices to convert the propagating pressure wave to the propagating
+            Mach_Plus = zeros(obj.NrPorts);
+            Mach_Min = zeros(obj.NrPorts);
+            for ii = 1:obj.NrPorts
+                Mach_Plus(ii,ii) = (1-MachNumberPort(ii))*sqrt(pi*Radius(ii)^2);
+                Mach_Min(ii,ii) = (1+MachNumberPort(ii))*sqrt(pi*Radius(ii)^2);
+            end
+            %Reshape the scattering matrix and calculate the scattering matrix
+            %w.r.t to power.
+            for ff = 1:length(obj.FreqVec)
+                for ii = 1:obj.NrPorts
+                    for jj = 1:obj.NrPorts
+                        S(ii,jj) = obj.ScatNPort.(['S',num2str(ii),num2str(jj)])(ff);
+                    end
+                end
+                S_Power(ff,:,:) = Mach_Min*S*inv(Mach_Plus);
+            end
+            S_diss = zeros(length(obj.FreqVec),obj.NrPorts);
+            
+            
+            for ii = 1:obj.NrPorts
+                for jj = 1:obj.NrPorts
+                    S_diss(:,ii) = S_diss(:,ii) + abs(S_Power(:,jj,ii)).^2;
+                end
+            end
+            
+            %Show the power dissipated per port
+            figure;
+            plot(obj.FreqVec,S_diss(:,1)-1,'.','MarkerSize',10)
+            hold on
+            plot(obj.FreqVec,S_diss(:,2)-1,'r.','MarkerSize',10)
+            legend('Port1','Port2')
+            if ~isempty(varargin)
+                fileName = varargin{1};
+                
+                %Write each variance distribuion of the scattering
+                %coefficients to a file.
+                fileID = fopen(fileName,'w');
+                
+                %Writing header
+                fprintf(fileID,'f \t');
+                for ii = 1:obj.NrPorts
+                    fprintf(fileID,['Port',num2str(ii),'\t']);
+                end
+                fprintf(fileID,'\n');
+                %Writing columns to the file
+                for ff = 1:length(obj.FreqVec)
+                    fprintf(fileID,'%e \t',obj.FreqVec(ff));
+                    for ii = 1:obj.NrPorts
+                        fprintf(fileID,'%e',S_diss(ff,ii));
+                        if ii ~= obj.NrPorts
+                            fprintf(fileID,'\t');
+                        else
+                            fprintf(fileID,'\n');
+                        end
+                    end
+                end
+                fclose(fileID);
+            end
+        end
+        
+        
+        
+        
         function obj = calculateDissipation(obj)
             %Obtain the mean value of the flow speed for each port.
             for ii = 1:obj.NrPorts
@@ -102,7 +187,7 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
                         S(ii,jj) = obj.ScatNPort.(['S',num2str(ii),num2str(jj)])(ff);
                     end
                 end
-                S_Power = inv(Mach_Min)*S*Mach_Plus;
+                S_Power = Mach_Min*S*inv(Mach_Plus);
                 lambda(:,ff) = eig(S_Power'*S_Power);
                 
             end
@@ -154,7 +239,7 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
             end
             for ii = 1:size(S,1)
                 for jj = 1:size(S,2)
-                    obj.ScatNPort.(['S',num2str(jj),num2str(ii)]) = reshape(S(ii,jj,:),1,[]);
+                    obj.ScatNPort.(['S',num2str(ii),num2str(jj)]) = reshape(S(ii,jj,:),1,[]);
                 end
             end
         end
@@ -169,16 +254,33 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
             for ii = 1:obj.NrPorts
                 for jj = 1:obj.NrPorts
                     subplot(obj.NrPorts,obj.NrPorts, (ii - 1)*obj.NrPorts + jj )
-                    plot(obj.FreqVec, abs( obj.ScatNPort.(['S',num2str(jj),num2str(ii)]) ))
+                    plot(obj.FreqVec, abs( obj.ScatNPort.(['S',num2str(ii),num2str(jj)]) ))
                 end
             end
             figure;
             for ii = 1:obj.NrPorts
                 for jj = 1:obj.NrPorts
                     subplot(obj.NrPorts,obj.NrPorts, (ii - 1)*obj.NrPorts + jj )
-                    plot(obj.FreqVec, unwrap(angle( obj.ScatNPort.(['S',num2str(jj),num2str(ii)]) ))*180/pi)
+                    plot(obj.FreqVec, unwrap(angle( obj.ScatNPort.(['S',num2str(ii),num2str(jj)]) ))*180/pi)
                 end
             end
+        end
+        
+        function displayTransferMatrix(obj)
+            %Calculate the scattering transfer matrix
+            for ff = 1:length(obj.FreqVec)
+                Tm(1,1) = -1/obj.ScatNPort.S12(ff)*(obj.ScatNPort.S22(ff)*obj.ScatNPort.S11(ff)-obj.ScatNPort.S12(ff)*obj.ScatNPort.S21(ff));
+                Tm(1,2) =  1/obj.ScatNPort.S12(ff)*obj.ScatNPort.S22(ff);
+                Tm(2,1) = -1/obj.ScatNPort.S12(ff)*obj.ScatNPort.S11(ff);
+                Tm(2,2) =  1/obj.ScatNPort.S12(ff);
+
+                ConvMat_r = [1 1; 1 -1];
+                T(:,:,ff) = ConvMat_r*Tm*inv(ConvMat_r);
+                assignin('base','T',T)
+            end
+             
+%             figure; plot(obj.FreqVec, real(Z_1)); hold on; plot(obj.FreqVec, real(Z_2),'-.')
+%             figure; plot(obj.FreqVec, imag(Z_1)); hold on; plot(obj.FreqVec, imag(Z_2),'-.')
         end
         function exportData_AbsAngle(obj,DataFile)
             fileID = fopen(DataFile,'w');
@@ -200,7 +302,7 @@ classdef NPortAnalysis  < matlab.mixin.SetGet
                 fprintf(fileID,'%e',obj.FreqVec(ff));
                 for ii = 1:obj.NrPorts
                     for jj = 1:obj.NrPorts
-                        ScatElement = obj.ScatNPort.(['S',num2str(jj),num2str(ii)]);
+                        ScatElement = obj.ScatNPort.(['S',num2str(ii),num2str(jj)]);
                         Angle = unwrap(angle(ScatElement))*180/pi;
                         fprintf(fileID,'\t %e \t %e \t %e \t %e',abs(ScatElement(ff)),Angle(ff));
                     end
