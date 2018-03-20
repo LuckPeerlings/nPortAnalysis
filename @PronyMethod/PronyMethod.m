af0b559d76821c92f0c3df6442f975f75a93c681
Luck Peerlings <luck@kth.se>
2018-03-20 10:34:52 +0100

    Added a function to plot the obtained wavenumber in the complex domain and show the direction of increasing frequencies using arrows

--- @PronyMethod/PronyMethod.m
+++ @PronyMethod/PronyMethod.m
@@ -34,26 +34,45 @@ classdef PronyMethod < handle
             end
         end
         
-        function obj = PlotWaveNumber(obj)
+        function obj = PlotWaveNumberComplexDomain(obj,ModeNr)
+            nn = ModeNr;
+            
+            [c,I] = sort(obj.Frequency);
+            I
+            figure
+            AX  = axes();
+            hold on;
+            PronyMethod.ArrowPlotComplexDomain(AX,obj.kappa(nn,I))
+            axis square
+            
+            ylabel('Imag. part of wavenumber')  
+            xlabel('Real part of wavenumber')
+           
+        end
+        function obj = PlotAmplitude(obj)
             %Function to plot the obtained real and imaginary part of each
             %of the modes and the relative amplitude of the modes compared to the largest mode present. 
+            
+            figure
             for nn = 1:obj.NrModes
                 subplot(obj.NrModes+1,1,nn)
-                plot(obj.Frequency,real(obj.Amplitude(nn,:)),'x');
+                plot(obj.Frequency,real(obj.kappa(nn,:)),'x');
                 hold all
-                plot(obj.Frequency,imag(obj.Amplitude(nn,:)),'o');
+                plot(obj.Frequency,imag(obj.kappa(nn,:)),'o');
             end
             legend('Real part of wavenumber','Imag. part of wavenumber')  
             
+           
             AX1 = subplot(obj.NrModes+1,1,obj.NrModes+1);            
             for ff = 1:size(obj.Frequency,2)
                 plot(obj.Frequency(ff),abs(obj.Amplitude(:,ff))/norm(obj.Amplitude(:,ff)),'x')
                 hold on         
                 AX1.ColorOrderIndex = 1;
-                ylabel('RelAmplitude')
+                ylabel('Rel. Amplitude')
                 xlabel('Frequency')
             end
         end
+        
         function TestClass(obj)
             %Function to determine whether the prony method is implemented
             %correctly
@@ -62,29 +81,59 @@ classdef PronyMethod < handle
             X = 0:MicSpacing:1-MicSpacing;
             %The wavenumbers have to be large enough that there is a
             %variation accross the microphones
-            k = [5,3,2,1]*10;
-            A = [5,3,2,1];
+            
+            %The wavenumbers are be sorted using the size of the absolute
+            %value.
+            k = [1i,2,2+1i,1]*10;
+            A = [15+9i,3-9i,2+2i,1-1i];
+            
             %Determine pressure with the above exponentials
             Pressure = A(1)*exp(1i*k(1)*X) + A(2)*exp(1i*k(2)*X) + A(3)*exp(1i*k(3)*X) + A(4)*exp(1i*k(4)*X);
             [C,kappa] = obj.PronyCoefficients(Pressure,MicSpacing,NrModes);
+            
             %Sort the modes by amplitude
             figure
-            subplot(3,1,1)
-            plot(X,real(Pressure))
+            subplot(3,2,1)
+            plot(X,real(Pressure));
             xlabel('Distance')
             ylabel('Real Pressure')
-            subplot(3,1,2)
+
+            subplot(3,2,2)
+            plot(X,imag(Pressure))
+            xlabel('Distance')
+            ylabel('Imag Pressure')
+            
+            subplot(3,2,3)
             plot(real(kappa),'x')
             hold all
-            plot(k,'o')
-            ylabel('Wavenumber')
+            plot(real(k),'o')
+            ylabel('Real Wavenumber')
             xlabel('Mode nr')
-            subplot(3,1,3)
+            legend('Input','Educed')
+            
+            subplot(3,2,4)
+            plot(imag(kappa),'x')
+            hold all
+            plot(imag(k),'o')
+            ylabel('Real Wavenumber')
+            xlabel('Mode nr')   
+            legend('Input','Educed')
+                        
+            subplot(3,2,5)
             plot(real(A),'x')
             hold all
             plot(real(C),'o')
-            ylabel('Amplitude')
+            ylabel('Real Amplitude')
             xlabel('Mode nr')
+            legend('Input','Educed')
+            
+            subplot(3,2,6)
+            plot(imag(A),'x')
+            hold all
+            plot(imag(C),'o')
+            ylabel('Imag Amplitude')
+            xlabel('Mode nr')
+            legend('Input','Educed')
         end
     end
     
@@ -117,6 +166,23 @@ classdef PronyMethod < handle
             [C,I] = sort(abs(C),'descend');
             Amplitude=Amplitude(I);
             kappa=kappa(I);
+        end
+        
+        function [AX] = ArrowPlotComplexDomain(AX,Data)
+            
+            for ii = 1:length(Data)-1
+                if ii == 1
+                quiver(real(Data(ii)),imag(Data(ii)),...
+                       real(Data(ii+1)-Data(ii)), ...
+                       imag(Data(ii+1)-Data(ii)),0,'o','filled','MarkerFaceColor','r','MaxHeadSize',0.5)
+                else
+                                    quiver(real(Data(ii)),imag(Data(ii)),...
+                       real(Data(ii+1)-Data(ii)), ...
+                       imag(Data(ii+1)-Data(ii)),0,'ko','filled','MaxHeadSize',0.5)
+                end                
+            end
+            plot(real(Data(ii+1)),imag(Data(ii+1)),'rx')
+                       
         end
     end
     
