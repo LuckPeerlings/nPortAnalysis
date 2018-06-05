921e2050250728fc825e2c4d4407be3ca9a1e368
Luck Peerlings <luck@kth.se>
2018-06-05 11:40:36 +0200

    Included a gui to select directories and save the determined impedances in the processed data folders

--- TestScripts/PronyMethod/ImpedanceProny.m
+++ TestScripts/PronyMethod/ImpedanceProny.m
@@ -1,45 +1,50 @@
 
-addpath('..\')
-addpath('..\Tools')
-addpath('..\Methods')
+addpath('.\..\')
+addpath('.\..\Tools')
+addpath('.\..\Methods')
 
 % close all
 clear all
+%Ask for the output directory and the folders with the input data
+DATADIRS = uipickfiles();
+for dd = 1:length(DATADIRS)
+    DATADIR = [DATADIRS{dd},'\'];
+    WaveDirections = {'Upstream','Downstream'};
+    
+    for ww = 1:length(WaveDirections) 
+        WaveDirection = WaveDirections{ww};
+        if strcmp(WaveDirection,'Upstream')
+            load([DATADIR,'ComplexPressures_Source1.mat'])
+            load([DATADIR,'MeasurementInfo.mat'])
+            SortedFrequencyIndex = Measurement.fIndex.Rep001(:,1);
+        elseif  strcmp(WaveDirection,'Downstream')
+            load([DATADIR,'ComplexPressures_Source2.mat'])
+            load([DATADIR,'MeasurementInfo.mat'])
+            SortedFrequencyIndex = Measurement.fIndex.Rep001(:,2);
+        else
+            error('WaveDirection incorrectly defined')
+        end
+        Epsilon = 25e-3;
+        X = 0:0.055:9*0.055;
 
-DATADIR = '.\ProcessedData\LinerC_Sample3_NoFlow_10Pa_Inserts_WideFrequency\';
+        MicPositions = X.';
+        MicEqPositions = X.';  
+        P = MeasData.Z(4:end-3, SortedFrequencyIndex );
+        if strcmp(WaveDirection,'Upstream')
+            FlowVelocity = Measurement.U*Measurement.WaveCal.Velocity_Corr.Port1;
+        elseif  strcmp(WaveDirection,'Downstream')
+            FlowVelocity = Measurement.U*Measurement.WaveCal.Velocity_Corr.Port2;
+        else
+            error('WaveDirection incorrectly defined')
+        end
+        Temperature = Measurement.t;
+        Height = 0.025;
+        Prony = PronyImpedance( Measurement.f.',P,MicPositions,MicEqPositions,5,Epsilon,...
+                                FlowVelocity, Temperature, Height);
 
-WaveDirection = 'Upstream';
-% WaveDirection = 'Downstream';
 
-if strcmp(WaveDirection,'Upstream')
-    load([DATADIR,'ComplexPressures_Source1.mat'])
-    load([DATADIR,'MeasurementInfo.mat'])
-    SortedFrequencyIndex = Measurement.fIndex.Rep001(:,1);
-elseif  strcmp(WaveDirection,'Downstream')
-    load([DATADIR,'ComplexPressures_Source2.mat'])
-    load([DATADIR,'MeasurementInfo.mat'])
-    SortedFrequencyIndex = Measurement.fIndex.Rep001(:,2);
-else
-    error('WaveDirection incorrectly defined')
+        Prony.CalculateImpedance;
+        Prony.PlotImpedance;
+        savefig([DATADIR,WaveDirection,'.fig'])
+    end
 end
-Epsilon = 25e-3;
-X = 0:0.055:9*0.055;
-
-MicPositions = X.';
-MicEqPositions = X.';  
-P = MeasData.Z(4:end-3, SortedFrequencyIndex );
-if strcmp(WaveDirection,'Upstream')
-    FlowVelocity = Measurement.U*Measurement.WaveCal.Velocity_Corr.Port1;
-elseif  strcmp(WaveDirection,'Downstream')
-    FlowVelocity = Measurement.U*Measurement.WaveCal.Velocity_Corr.Port2;
-else
-    error('WaveDirection incorrectly defined')
-end
-Temperature = Measurement.t;
-Height = 0.025;
-Prony = PronyImpedance( Measurement.f.',P,MicPositions,MicEqPositions,5,Epsilon,...
-                        FlowVelocity, Temperature, Height);
-
-
-Prony.CalculateImpedance;
-Prony.PlotImpedance;
